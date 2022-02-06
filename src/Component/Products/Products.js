267bbe63779dfe4fa731f31fa9bfdb097cd6c4e1
emonislam079 <emonislam079@gmail.com>
2022-02-06 14:22:36 +0600

    product

--- src/Component/Products/Products.js
+++ src/Component/Products/Products.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 
 const Products = () => {
+    const [product , setProduct]= useState([]);
+    useEffect(()=>{
+        fetch('https://assessment-edvora.herokuapp.com/')
+        .then(res =>res.json())
+        .then(data => setProduct(data))
+    },[]);
+
     return (
         <div>
-            <h1 className='text-light'>This is products</h1>
+            <Row xs={1} md={4} className="g-4">
+            {
+                product.map(product =><div
+                product ={product}
+                >
+    <Col>
+      <Card>
+        <Card.Img variant="top" src={product.image} width='70px' height='70px' />
+        <Card.Body>
+          <Card.Title>{product.product_name}</Card.Title>
+          <Card.Text>
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+    </div>)
+            }
+  
+    
+</Row>
         </div>
     );
 };
