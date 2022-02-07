7f4614d22ce42b03957bd5f389ec8eb1c7e3500f
emonislam079 <emonislam079@gmail.com>
2022-02-07 14:10:20 +0600

    filter

--- src/App.js
+++ src/App.js
@@ -1,75 +1,260 @@
-import {Col, Container, Dropdown, Row } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import {Carousel, Col, Container, Dropdown, Row } from 'react-bootstrap';
 import './App.css';
-import Products from './Component/Products/Products';
 
 function App() {
+  const [product , setProduct]= useState([]);
+    useEffect(()=>{
+        fetch('https://assessment-edvora.herokuapp.com/')
+        .then(res =>res.json())
+        .then(data => setProduct(data))
+    },[]);
   return (
-    <Container className='App'>
+<Container className='App'>
   <Row>
-  <Row>
-    <Col xs={12} md={3} >
-      <div className='filter'>
-          <h1 className='filter-left'>Filters</h1>
-          <hr></hr>
-        <div>
+      <Row>
+          <Col xs={12} md={3} >
+            <div className='filter'>
+                <h1 className='filter-left'>Filters</h1>
+                <hr></hr>
+              <div>
+              <Dropdown className='text-center'>
+          <Dropdown.Toggle className='drop' id="dropdown-button-dark-example1">
+            Products
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu variant="dark">
+            <Dropdown.Item href="#apple" active>
+              Apple
+            </Dropdown.Item>
+            <Dropdown.Item href="#alibaba">Alibaba</Dropdown.Item>
+            
+            
+          </Dropdown.Menu>
+        </Dropdown>
+
         <Dropdown className='text-center'>
-    <Dropdown.Toggle className='drop' id="dropdown-button-dark-example1">
-      Products
-    </Dropdown.Toggle>
+          <Dropdown.Toggle className='drop' id="dropdown-button-dark-example1">
+            State
+          </Dropdown.Toggle>
 
-    <Dropdown.Menu variant="dark">
-      <Dropdown.Item href="#/action-1" active>
-        Action
-      </Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      
-    </Dropdown.Menu>
-  </Dropdown>
+          <Dropdown.Menu variant="dark">
+            <Dropdown.Item href="#/action-1" active>
+              Action
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            
+            
+          </Dropdown.Menu>
+        </Dropdown>
+        <Dropdown className='text-center'>
+          <Dropdown.Toggle className='drop' id="dropdown-button-dark-example1">
+            City
+          </Dropdown.Toggle>
 
-  <Dropdown className='text-center'>
-    <Dropdown.Toggle className='drop' id="dropdown-button-dark-example1">
-      State
-    </Dropdown.Toggle>
+          <Dropdown.Menu variant="dark">
+            <Dropdown.Item href="#/action-1" active>
+              Action
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      </div>
+          </Col>
+          <Col xs={12} md={9} className="Product-container" >
+            <h1 className='title'>Edvora</h1>
+            <h1 className='products'>Products</h1>
+            <h1 id="apple" className='productName'>Apple</h1>
+            <hr className='hr'></hr>
+            <div>
+                  <Carousel>
+                          <Carousel.Item>
+                          <Row xs={1} md={3} className="g-4 Product-Container">
+                  {
+                      product.filter(product=> product.brand_name = "Apple").slice(0,3).map(product =><div
+                      product ={product}
+                      >
 
-    <Dropdown.Menu variant="dark">
-      <Dropdown.Item href="#/action-1" active>
-        Action
-      </Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      
-    </Dropdown.Menu>
-  </Dropdown>
-  <Dropdown className='text-center'>
-    <Dropdown.Toggle className='drop' id="dropdown-button-dark-example1">
-      City
-    </Dropdown.Toggle>
-
-    <Dropdown.Menu variant="dark">
-      <Dropdown.Item href="#/action-1" active>
-        Action
-      </Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
-</div>
-</div>
-    </Col>
-    <Col xs={12} md={9} className="Product-container" >
-      <h1 className='title'>Edvora</h1>
-      <h1 className='products'>Products</h1>
-      <h1 className='productName'>Product Name</h1>
-      <hr className='hr'></hr>
-      <Products></Products>
-      <h1 className='productName2'>Product Name</h1>
-      <hr className='hr'></hr>
-      <Products></Products>
-    </Col>
+                        <div className='Single-product'>
+                        <Container>
+                                  <Row>
+                                    <Col md={5}><img src={product.image} alt="" height='70px' width='70px'/>
+                                    <p className='location'>Location: {product.address.state}</p>
+                                    </Col>
+                                    <Col md={7}>
+                                    <h1 className='ProductName'>{product.product_name}</h1>
+                                    <h1 className='brandName'>{product.brand_name}</h1>
+                                    <h1 className='price'><span>$ </span>{product.price}</h1>
+                                    <h1 className='Date'><span id='date'>Date: </span>{product.date.slice(0,10)}</h1>               
+                                    </Col>
+                                    <p className='discription'>Discription: {product.discription}</p>
+                                  </Row>
+                                </Container>
+                        </div>
+              </div>)
+                  }
   </Row>
-  </Row>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                    <Row xs={1} md={3} className="g-4 Product-Container">
+            {
+                product.filter(product=> product.brand_name = "Apple").slice(7,9).map(product =><div
+                product ={product}
+                >
+
+                  <div className='Single-product'>
+                  <Container>
+                            <Row>
+                              <Col md={5}><img src={product.image} alt="" height='70px' width='70px'/>
+                              <p className='location'>Location: {product.address.state}</p>
+                              </Col>
+                              <Col md={7}>
+                              <h1 className='ProductName'>{product.product_name}</h1>
+                              <h1 className='brandName'>{product.brand_name}</h1>
+                              <h1 className='price'><span>$ </span>{product.price}</h1>
+                              <h1 className='Date'><span id='date'>Date: </span>{product.date.slice(0,10)}</h1>               
+                              </Col>
+                              <p className='discription'>Discription: {product.discription}</p>
+                            </Row>
+                          </Container>
+                  </div>
+        </div>)
+            }
   
+    
+</Row>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                    <Row xs={1} md={3} className="g-4 Product-Container">
+            {
+                product.filter(product=> product.brand_name = "Apple").slice(4,6).map(product =><div
+                product ={product}
+                >
+
+                  <div className='Single-product'>
+                  <Container>
+                            <Row>
+                              <Col md={5}><img src={product.image} alt="" height='70px' width='70px'/>
+                              <p className='location'>Location: {product.address.state}</p>
+                              </Col>
+                              <Col md={7}>
+                              <h1 className='ProductName'>{product.product_name}</h1>
+                              <h1 className='brandName'>{product.brand_name}</h1>
+                              <h1 className='price'><span>$ </span>{product.price}</h1>
+                              <h1 className='Date'><span id='date'>Date: </span>{product.date.slice(0,10)}</h1>               
+                              </Col>
+                              <p className='discription'>Discription: {product.discription}</p>
+                            </Row>
+                          </Container>
+                  </div>
+        </div>)
+            }
+  
+    
+</Row>
+                    </Carousel.Item>
+                    </Carousel>
+            
+        </div>
+      <h1 id="alibaba" className='productName2'>Alibaba</h1>
+      <hr className='hr'></hr>
+      <div>
+            <Carousel>
+                    <Carousel.Item>
+                    <Row xs={1} md={3} className="g-4 Product-Container">
+            {
+                product.filter(product=> product.brand_name = "Alibaba").slice(0,3).map(product =><div
+                product ={product}
+                >
+
+                  <div className='Single-product'>
+                  <Container>
+                            <Row>
+                              <Col md={5}><img src={product.image} alt="" height='70px' width='70px'/>
+                              <p className='location'>Location: {product.address.state}</p>
+                              </Col>
+                              <Col md={7}>
+                              <h1 className='ProductName'>{product.product_name}</h1>
+                              <h1 className='brandName'>{product.brand_name}</h1>
+                              <h1 className='price'><span>$ </span>{product.price}</h1>
+                              <h1 className='Date'><span id='date'>Date: </span>{product.date.slice(0,10)}</h1>               
+                              </Col>
+                              <p className='discription'>Discription: {product.discription}</p>
+                            </Row>
+                          </Container>
+                  </div>
+        </div>)
+            }
+  
+    
+</Row>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                    <Row xs={1} md={3} className="g-4 Product-Container">
+            {
+                product.filter(product=> product.brand_name = "Alibaba").slice(7,9).map(product =><div
+                product ={product}
+                >
+
+                  <div className='Single-product'>
+                  <Container>
+                            <Row>
+                              <Col md={5}><img src={product.image} alt="" height='70px' width='70px'/>
+                              <p className='location'>Location: {product.address.state}</p>
+                              </Col>
+                              <Col md={7}>
+                              <h1 className='ProductName'>{product.product_name}</h1>
+                              <h1 className='brandName'>{product.brand_name}</h1>
+                              <h1 className='price'><span>$ </span>{product.price}</h1>
+                              <h1 className='Date'><span id='date'>Date: </span>{product.date.slice(0,10)}</h1>               
+                              </Col>
+                              <p className='discription'>Discription: {product.discription}</p>
+                            </Row>
+                          </Container>
+                  </div>
+        </div>)
+            }
+  
+    
+</Row>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                    <Row xs={1} md={3} className="g-4 Product-Container">
+            {
+                product.filter(product=> product.brand_name = "Alibaba").slice(4,6).map(product =><div
+                product ={product}
+                >
+
+                  <div className='Single-product'>
+                  <Container>
+                            <Row>
+                              <Col md={5}><img src={product.image} alt="" height='70px' width='70px'/>
+                              <p className='location'>Location: {product.address.state}</p>
+                              </Col>
+                              <Col md={7}>
+                              <h1 className='ProductName'>{product.product_name}</h1>
+                              <h1 className='brandName'>{product.brand_name}</h1>
+                              <h1 className='price'><span>$ </span>{product.price}</h1>
+                              <h1 className='Date'><span id='date'>Date: </span>{product.date.slice(0,10)}</h1>               
+                              </Col>
+                              <p className='discription'>Discription: {product.discription}</p>
+                            </Row>
+                          </Container>
+                  </div>
+        </div>)
+            }
+  
+    
+</Row>
+                    </Carousel.Item>
+                    </Carousel>
+            
+        </div>
+    </Col>
+  </Row>
+  </Row>
 </Container>
   );
 }
